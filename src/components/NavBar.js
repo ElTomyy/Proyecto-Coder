65f76933379cc39d244757394dc98d06d75add1d
ElTomyy <tomasirioyen@gmail.com>
2023-09-10 16:56:13 -0300

    first commit

---
+++ src/components/NavBar.js
@@ -0,0 +1,17 @@
+import "bulma/css/bulma.css"
+
+function NavBar() {
+
+    return (
+        <div>
+            <h1>Easy Shop</h1>
+            <ul>
+                <li><a>Opcion uno</a></li>
+                <li><a>Opcion dos</a></li>
+                <li><a>Opcion tres</a></li>
+            </ul>
+        </div>
+    )
+}
+
+export default NavBar
